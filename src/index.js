6447b5b269a003b4223be500d3a0ff4dd6da149e
Alex Sandiiarov <sandiyarov@gmail.com>
2018-11-21 12:28:18 -0600

    fix: 🐛 fix warning message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import warning from 'warning';
 
 export default () => {
-  if (process.env.NODE_ENV !== 'production') {
-    warning(
-      true,
-      'You likely forgot to add "use-branch/babel" to your babel config'
-    );
-  }
+  warning(
+    process.env.NODE_ENV === 'production',
+    'You likely forgot to add "use-branch/babel" to your babel config'
+  );
 };
